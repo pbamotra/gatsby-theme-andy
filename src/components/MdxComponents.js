0a577f098ce4b52e1719a2b61e975d947b8b5e4e
Pankesh Bamotra <pankesh.bamotra@gmail.com>
2021-06-09 16:20:32 -0700

    [BLOG-0] Hardcoded blog name for references

--- src/components/MdxComponents.js
+++ src/components/MdxComponents.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import React from 'react';
-import { LinkToStacked } from './CustomLinkToStacked';
+import { LinkToStacked } from 'react-stacked-pages-hook';
 import { Link } from 'gatsby';
 import { jsx } from 'theme-ui';
 
@@ -10,10 +10,10 @@ const AnchorTag = ({ href, popups = {}, noPopups = false, ...restProps }) => {
   if (!href) href = restProps.to;
   if (!href.match(/^http/))
     return noPopups ? (
-      <Link {...restProps} to={href} sx={{ variant: 'links.internal' }} />
+      <Link {...restProps} to={`https://pankesh.com${href}`} sx={{ variant: 'links.internal' }} />
     ) : (
       <Tippy content={popups[href.replace(/^\//, '')]} placement="right" animation="shift-away">
-        <LinkToStacked {...restProps} to={href} sx={{ variant: 'links.internal' }} />
+        <LinkToStacked {...restProps} to={`https://pankesh.com${href}`} sx={{ variant: 'links.internal' }} />
       </Tippy>
     );
   return <a {...restProps} href={href} />;
