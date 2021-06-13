419498768421989c0a50729354ddef2e6ce55f3f
Pankesh Bamotra <pankesh.bamotra@gmail.com>
2021-06-13 11:26:15 -0700

    [BLOG-0] Hardcodes pankesh.com to second brain page

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ import { Box, jsx } from 'theme-ui';
 export default ({ siteMetadata }) => (
   <header>
     <Box py={2} px={3} sx={{ borderBottom: '1px solid', borderColor: 'gray' }}>
-      <Link to="/" sx={{ fontWeight: 'bold', color: 'text', textDecoration: 'none' }}>
+      <Link to="https://www.pankesh.com" sx={{ fontWeight: 'bold', color: 'text', textDecoration: 'none' }}>
         {siteMetadata.title}
       </Link>
     </Box>
