d823a58949508a442eccf63e2bf250ca1276cbf6
Pankesh Bamotra <pankesh.bamotra@gmail.com>
2021-06-21 10:46:13 -0700

    [BLOG-0] Fixes font awesome flickering issue - https://medium.com/@fabianterh/fixing-flashing-huge-font-awesome-icons-on-a-gatsby-static-site-787e1cfb3a18

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,17 @@ import { Link } from 'gatsby';
 import { Box, jsx } from 'theme-ui';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
+import { config } from "@fortawesome/fontawesome-svg-core"
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
+config.autoAddCss = false;
+const home = <FontAwesomeIcon icon={faHome} />;
 
 export default ({ siteMetadata }) => (
   <header>
     <Box py={2} px={3} sx={{ borderBottom: '1px solid', borderColor: 'gray', fontStyle: 'italic'}}>
       <Link to="https://www.pankesh.com" sx={{ fontWeight: 'bold', color: 'text', textDecoration: 'none' }}>
-        {siteMetadata.title} <FontAwesomeIcon icon={faHome} />
+        {siteMetadata.title} {home}
       </Link>
     </Box>
   </header>
