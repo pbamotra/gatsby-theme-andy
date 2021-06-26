dbb0402f4c491174cda95ce63740387371cb227e
Pankesh Bamotra <pankesh.bamotra@gmail.com>
2021-06-26 10:02:38 -0700

    [BLOG-0] No italics header on notes

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,12 @@ import { config } from "@fortawesome/fontawesome-svg-core"
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 config.autoAddCss = false;
-const home = <FontAwesomeIcon icon={faHome} />;
 
 export default ({ siteMetadata }) => (
   <header>
-    <Box py={2} px={3} sx={{ borderBottom: '1px solid', borderColor: 'gray', fontStyle: 'italic'}}>
+    <Box py={2} px={3} sx={{ borderBottom: '1px solid', borderColor: 'gray'}}>
       <Link to="https://www.pankesh.com" sx={{ color: 'text', textDecoration: 'none', fontWeight: 'bold', }}>
-        {siteMetadata.title} {home}
+        {siteMetadata.title}
       </Link>
       <Link to="/" sx={{ color: 'text', textDecoration: 'underline', marginLeft: '30px' }}>
         {"Notes home"} 
