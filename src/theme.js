b5e00e2ee2d2781c7c5742c6f19318cc8c7586c5
Pankesh Bamotra <pankesh.bamotra@gmail.com>
2021-06-26 02:08:26 -0700

    [BLOG-0] Anchor link color is consitent with rest of the website

--- src/theme.js
+++ src/theme.js
@@ -7,7 +7,7 @@ export default {
     'text-light': '#718096',
     background: '#fff',
     primary: '#3182ce',
-    links: '#3182ce',
+    links: '#1e0b74',
     gray: '#dadada',
     accent: '#fafafc',
   },
