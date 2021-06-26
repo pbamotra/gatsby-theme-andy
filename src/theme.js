bb5c0d28c9493e71bf20587d2f7bc8718e1c54b0
Pankesh Bamotra <pankesh.bamotra@gmail.com>
2021-06-26 02:15:02 -0700

    [BLOG-0] Revert anchor color

--- src/theme.js
+++ src/theme.js
@@ -7,7 +7,7 @@ export default {
     'text-light': '#718096',
     background: '#fff',
     primary: '#3182ce',
-    links: '#1e0b74',
+    links: '#3182ce',
     gray: '#dadada',
     accent: '#fafafc',
   },
