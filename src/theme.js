9fc0822d81a6a63e44b935bcecc6231825afd28a
Pankesh Bamotra <pankesh.bamotra@gmail.com>
2021-07-16 16:37:54 -0700

    [BLOG-0] Uniform anchor color everywhere

--- src/theme.js
+++ src/theme.js
@@ -7,7 +7,7 @@ export default {
     'text-light': '#718096',
     background: '#fff',
     primary: '#3182ce',
-    links: '#0A84FF',
+    links: '#3344dd',
     gray: '#dadada',
     accent: '#fafafc',
   },
